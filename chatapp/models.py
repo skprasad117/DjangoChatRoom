a09b5c4874956752a189b358984ccb6558450494
skprasad117 <skprasad117@gmail.com>
2023-07-27 14:01:59 +0530

    one to one chat basic structure implemented

--- chatapp/models.py
+++ chatapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class ChatRoom(models.Model):
+    roomname = models.CharField(max_length=30)
+    messages = models.TextField()
--- chatapp/views.py
+++ chatapp/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+from .models import ChatRoom
 # Create your views here.
 def chatPage(request,roomname=None):
     if not request.user.is_authenticated:
         return redirect("login-user")
     if roomname is None:
         roomname = "PublicChatRoom"
+    elif roomname is not "PublicChatRoom":
+        user_instance = User.objects.get(username=roomname)
+        if user_instance.pk > request.user.pk:
+            roomname = request.user.username +"-"+ roomname
+        elif user_instance.pk < request.user.pk: 
+            roomname = roomname +"-"+request.user.username
+   
+    print("type of ",type(roomname),roomname.isnumeric())
     print("roomname -----:", roomname)
     users = User.objects.all()
     context = {"users":users,"roomname":roomname}
