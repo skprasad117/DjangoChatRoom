26cb1a2e47a0ea06065f7ea37a35de11f78c3968
skprasad117 <skprasad117@gmail.com>
2023-07-27 20:02:11 +0530

    notifications feature added

--- chatapp/consumers.py
+++ chatapp/consumers.py
@@ -1,14 +1,18 @@
+import time
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.consumer import SyncConsumer
 
-
-from .models import ChatRoom
+from django.contrib.auth import get_user
+from django.contrib.auth.models import User
+from .models import ChatRoom, OnlineUsers
 from channels.db import database_sync_to_async
+
 class ChatConsumer(AsyncWebsocketConsumer):
 	async def connect(self):
-		
 		self.roomGroupName = self.scope['url_route']['kwargs']['roomname']
+		self.notificationgroup = "notification"
+		await database_sync_to_async(self.user_logged)(self.scope['user'])
 		
 		print("roomname .......",self.roomGroupName)
 		await self.channel_layer.group_add(
@@ -16,28 +20,63 @@ class ChatConsumer(AsyncWebsocketConsumer):
 			self.channel_name
 		)
 		print("channel_name",self.channel_name)
+		# await self.accept()
+
+		await self.channel_layer.group_add(
+			self.notificationgroup ,
+			self.channel_name
+		)
+		print("channel_name",self.channel_name)
 		await self.accept()
 		
 	async def disconnect(self , close_code):
+		await database_sync_to_async(self.user_disconnect)(self.scope['user'])
 		await self.channel_layer.group_discard(
 			self.roomGroupName ,
 			self.channel_layer
 		)
+		
+
+		await self.channel_layer.group_discard(
+			self.notificationgroup ,
+			self.channel_layer
+		)
+
 	async def receive(self, text_data):
 		text_data_json = json.loads(text_data)
 		message = text_data_json["message"]
 		username = text_data_json["username"]
+
+		reciver = text_data_json["reciver"]
+		reciver = reciver.replace("-","")
+		print("-------",reciver)
+		user = self.scope.get('user', None)
+		reciver = reciver.replace(user.username,"")
+		notification_message = f"{reciver} you recived a message from {user.username}"
+		print("-------",reciver)
+		print(user)
+
 		await self.channel_layer.group_send(
 			self.roomGroupName,{
 				"type" : "sendMessage" ,
+				"flag":"message",
 				"message" : message ,
 				"username" : username ,
 			})
+		
+		await self.channel_layer.group_send(
+			self.notificationgroup,{
+				"type" : "sendMessage" ,
+				"flag":"notification",
+				"message" : notification_message,
+				"username" : reciver ,
+			})
 		await database_sync_to_async(self.update_chat_history)(message,username)
 	async def sendMessage(self , event) :
+		flag = event["flag"]
 		message = event["message"]
 		username = event["username"]
-		await self.send(text_data = json.dumps({"message":message ,"username":username}))
+		await self.send(text_data = json.dumps({"message":message ,"username":username,"flag":flag}))
 
 
 
@@ -58,3 +97,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
 			chat_history_instancne.save()
 		print(message_string_to_update)
 		return None
+	
+	def user_logged(self,username):
+		all_users = OnlineUsers.objects.get_or_create(user = username)
+		all_users[0].status = True
+		all_users[0].save()
+		return None
+	def user_disconnect(self,username):
+		all_users = OnlineUsers.objects.get(user = username)
+		all_users.status = False
+		all_users.save()
+		return None
+	
+	def get_update(self):
+		logged_user_instance = OnlineUsers.objects.all()
+		return logged_user_instance
--- chatapp/models.py
+++ chatapp/models.py
@@ -1,6 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class ChatRoom(models.Model):
     roomname = models.CharField(max_length=30)
     messages = models.TextField()
+
+class OnlineUsers(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.BooleanField(default=False)
